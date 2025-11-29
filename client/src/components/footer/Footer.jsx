d545245047f1e17ab6f3a809222a5b4b38223c64
Vasilev-Stefan <sbasilev@gmail.com>
2025-11-29 02:43:09 +0200

    Add Footer component

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="text-white px-3 py-2" style={{ backgroundColor: '#222222' }}>
+            <div className="text-center text-gray-300 text-sm">
+                SoftUni React project
+            </div>
+        </footer>
+    );
+}
