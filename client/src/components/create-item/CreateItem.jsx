0875896cb80721cacd39e680226231543e48ec38
Vasilev-Stefan <sbasilev@gmail.com>
2025-12-10 20:17:10 +0200

    Add the ability to add item when authenticated

--- client/src/components/create-item/CreateItem.jsx
+++ client/src/components/create-item/CreateItem.jsx
@@ -1,4 +1,56 @@
+import { useNavigate } from "react-router"
+import { useFetch } from "../../hooks/useFetch"
+import { useForm } from "../../hooks/useForm"
+
 export function CreateItem() {
+    const { request } = useFetch()
+    const navigate = useNavigate()
+    const initialData = {
+        name: '',
+        description: '',
+        price: '',
+        category: 'sofas',
+        imageUrl: '',
+    }
+
+    const validator = (data) => {
+        const errors = {}
+        if(!data.name) {
+            errors.name = 'Product name is required'
+        }
+
+        if(!data.description) {
+            errors.description = 'Description is required'
+        }
+
+        if(!data.price) {
+            errors.price = 'Price is required'
+        }
+
+        if(!data.category) {
+            errors.category = 'Category is required'
+        }
+
+        if(!data.imageUrl) {
+            errors.imageUrl = 'Image is required'
+        }
+
+        return errors
+    }
+
+    const create = async (data) => {
+        try {
+            const result = await request('data/furniture', 'POST', data)
+            console.log(result)
+            navigate('/catalog')
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const { errors, onSubmitHandler, inputFiller } = useForm(initialData, create, validator)
+
+
     return (
         <div className=" bg-[#1a1a1a] text-white px-10 py-4" style={{
                 height: 'calc(100vh - 123px)'
@@ -12,7 +64,7 @@ export function CreateItem() {
             {/* Form Container */}
             <div className="max-w-3xl mx-auto bg-[#222222] p-10 rounded-2xl shadow-lg">
 
-                <form className="space-y-6">
+                <form className="space-y-6" action={onSubmitHandler}>
 
                     {/* Name */}
                     <div>
@@ -20,6 +72,7 @@ export function CreateItem() {
                             Product Name
                         </label>
                         <input
+                            {...inputFiller('name')}
                             type="text"
                             placeholder="e.g., Modern Sofa"
                             className="w-full px-4 py-2 bg-[#111111] text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
@@ -32,6 +85,7 @@ export function CreateItem() {
                             Description
                         </label>
                         <textarea
+                            {...inputFiller('description')}
                             placeholder="Short description of the item..."
                             rows="4"
                             className="w-full px-4 py-2 bg-[#111111] text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
@@ -44,6 +98,7 @@ export function CreateItem() {
                             Price ($)
                         </label>
                         <input
+                            {...inputFiller('price')}
                             type="number"
                             placeholder="e.g., 499"
                             className="w-full px-4 py-2 bg-[#111111] text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
@@ -56,15 +111,16 @@ export function CreateItem() {
                             Category
                         </label>
                         <select
+                            {...inputFiller('category')}
                             className="w-full px-4 py-2 bg-[#111111] text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
                         >
-                            <option>Sofas</option>
-                            <option>Tables</option>
-                            <option>Beds</option>
-                            <option>Chairs</option>
-                            <option>Lighting</option>
-                            <option>Storage</option>
-                            <option>Decor</option>
+                            <option value='sofas'>Sofas</option>
+                            <option value='tables'>Tables</option>
+                            <option value='beds'>Beds</option>
+                            <option value='chairs'>Chairs</option>
+                            <option value='lighting'>Lighting</option>
+                            <option value='storage'>Storage</option>
+                            <option value='decor'>Decor</option>
                         </select>
                     </div>
 
@@ -74,6 +130,7 @@ export function CreateItem() {
                             Image URL
                         </label>
                         <input
+                            {...inputFiller('imageUrl')}
                             type="text"
                             placeholder="https://example.com/image.jpg"
                             className="w-full px-4 py-2 bg-[#111111] text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
